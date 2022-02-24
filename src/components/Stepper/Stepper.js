8131b3b6657388163d46e97c20204e7a6da37f2b
nedosika <nedosika@gmail.com>
2022-02-24 14:58:00 +0200

    stepper? not work

---
+++ src/components/Stepper/Stepper.js
@@ -0,0 +1,12 @@
+import React, {useState} from 'react';
+
+const Stepper = ({children, activeStep}) => {
+
+    return (
+        <div>
+            {children.length}
+        </div>
+    );
+};
+
+export default Stepper;
