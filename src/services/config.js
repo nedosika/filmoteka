bfdf8f5042139baf172a5e2293ffd4b6c99c1611
nedosika <nedosika@gmail.com>
2022-04-03 09:09:04 +0300

    add formik validation to the add dialog with stepper

--- src/services/config.js
+++ src/services/config.js
@@ -1,2 +1,2 @@
-export const API_URL = 'https://radiant-badlands-09970.herokuapp.com';
-//export const API_URL = 'http://localhost:8080/api';
+export const API_URL = 'https://radiant-badlands-09970.herokuapp.com/api';
+//export const API_URL = 'http://localhost:5000/api';
