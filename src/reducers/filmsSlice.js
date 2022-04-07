86248ee0dac4bb043ac3ea9a080bfdeec3c841f2
nedosika <nedosika@gmail.com>
2022-04-07 22:43:11 +0300

    add error to store when error add film

--- src/reducers/filmsSlice.js
+++ src/reducers/filmsSlice.js
@@ -1,5 +1,5 @@
 import { createEntityAdapter, createSlice } from '@reduxjs/toolkit';
-import { getFilm, getFilms, updateFilm } from '../actions/filmsActions';
+import { addFilm, getFilm, getFilms, updateFilm } from '../actions/filmsActions';
 
 const filmsAdapter = createEntityAdapter({
   selectId: (film) => film.id,
@@ -9,6 +9,7 @@ export const slice = createSlice({
   name: 'films',
   initialState: filmsAdapter.getInitialState({
     loading: false,
+    error: null,
     current: {},
     pages: 0,
     page: 1,
@@ -16,15 +17,18 @@ export const slice = createSlice({
   extraReducers: {
     [getFilms.pending.type]: (state) => {
       state.loading = true;
+      state.error = null;
     },
     [getFilms.fulfilled.type]: (state, { payload }) => {
       state.loading = false;
+      state.error = null;
       state.pages = payload.pages;
       state.page = payload.page;
       filmsAdapter.setAll(state, payload.films);
     },
-    [getFilms.rejected.type]: (state, action) => {
+    [getFilms.rejected.type]: (state, { payload }) => {
       state.loading = false;
+      state.error = payload?.message;
     },
     [getFilm.fulfilled.type]: (state, { payload: film }) => {
       state.current = film;
@@ -34,6 +38,10 @@ export const slice = createSlice({
         id: film.id,
         changes: film,
       });
+    },
+    [addFilm.rejected.type]: (state, { payload }) => {
+      state.loading = false;
+      state.error = payload?.message;
     },
   },
 });
