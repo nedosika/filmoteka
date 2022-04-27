4284242ceee600606e32545f799781d7ad95a7ee
nedosika <nedosika@gmail.com>
2022-04-27 21:08:05 +0300

    add Providers

--- src/App.js
+++ src/App.js
@@ -1,18 +1,14 @@
 import React from 'react';
-import { Provider } from 'react-redux';
-import { DialogProvider } from './components/DialogManager/useDialog';
+import AllTheProviders from './components/AllTheProviders';
 import SnackStack from './components/SnackStack';
 import Router from './router';
-import store from './store';
 
 function App() {
   return (
-    <Provider store={store}>
-      <DialogProvider>
-        <Router />
-        <SnackStack />
-      </DialogProvider>
-    </Provider>
+    <AllTheProviders>
+      <Router />
+      <SnackStack />
+    </AllTheProviders>
   );
 }
 
