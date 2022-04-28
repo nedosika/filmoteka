dc817954ad49dd65afa10b93c733f763df24f3d1
nedosika <nedosika@gmail.com>
2022-04-29 00:42:42 +0300

    add test to the films page

--- src/pages/Films/Films.js
+++ src/pages/Films/Films.js
@@ -130,9 +130,13 @@ const Films = () => {
         </Box>
         <Grid container rowSpacing={1} columnSpacing={{ xs: 1, sm: 2, md: 3 }}>
           {isLoading
-            ? generateSkeletonsArray(filmsPerPage).map((element) => <FilmSkeleton />)
+            ? generateSkeletonsArray(filmsPerPage).map(({ id }) => (
+                <Grid item xs={12} sm={6} md={4} lg={3} xl={2} key={id}>
+                  <FilmSkeleton />
+                </Grid>
+              ))
             : films.map((film) => (
-                <Grid item xs={12} sm={6} md={4} lg={3} xl={2} key={film.id}>
+                <Grid item xs={12} sm={6} md={4} lg={3} xl={2} key={film.id} name="film">
                   <FilmCard film={film} />
                 </Grid>
               ))}
--- src/pages/Films/Films.test.js
+++ src/pages/Films/Films.test.js
@@ -1,7 +1,21 @@
 import React from 'react';
-import App from '../../App';
+import { BrowserRouter } from 'react-router-dom';
+import AllTheProviders from '../../components/AllTheProviders';
+import SnackStack from '../../components/SnackStack';
 import { render, screen } from '../../test-utils/test-library-utils';
+import Films from './Films';
 
-test('render films', async () => {
-  render(<App />);
+test('display image for each film from server', async () => {
+  render(
+    <AllTheProviders>
+      <BrowserRouter>
+        <Films />
+      </BrowserRouter>
+      <SnackStack />
+    </AllTheProviders>,
+  );
+
+  const filmImages = await screen.findAllByRole('img', { name: /film$/i });
+
+  expect(filmImages).toHaveLength(5);
 });
